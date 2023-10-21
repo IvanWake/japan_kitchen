c82e1cd03643aa6516bf582240ae56e74de17bd5
Ivan Wake <purplebaby16@vk.com>
2023-10-21 17:54:02 +0300

    Setup the environment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto py-10">
+      <h1 className="text-center text-3xl text-bold">Japan Kitchen</h1>
     </div>
   );
 }
