0185ee8d9dfb259ef3466b9bc7e6e0dc56f0d856
Ivan Wake <purplebaby16@vk.com>
2023-11-24 19:44:29 +0300

    Change PromoText

--- src/components/Meals/PromoText.js
+++ src/components/Meals/PromoText.js
@@ -1,7 +1,7 @@
 const PromoText = () => {
     return (
         <section className="text-center relative text-white max-w-3xl w-9/10 m-auto -mt-40 rounded-2xl p-7 bg-promo-color">
-            <h2 className="text-3xl mt-0 mb-3">Online Sushi Restaurant Japan Kitchen</h2>
+            <h2 className="text-3xl mt-0 mb-3">Online Sushi Restaurant "Japan Kitchen"</h2>
             <p>
                 Japan Kitchen - this is sushi-restaurant where there is sushi, sashimi and rolls and more dishes Japanese kitchen
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, obcaecati.
